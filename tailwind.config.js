026915a01de847b119bdc62863bffaf3353aaacd
Saqlain Ahmad <123837216+SaqlainAhmad786@users.noreply.github.com>
2024-12-12 17:34:26 +0530

    hi

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui'
+
 export default {
   content: [
     "./index.html",
@@ -12,7 +14,7 @@ export default {
     },
   },
   plugins: [
-    require('daisyui')
+    daisyui
   ],
 }
 
