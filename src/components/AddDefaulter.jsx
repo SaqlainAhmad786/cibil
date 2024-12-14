842d8b42782a765275ce85c923a377df556b08cb
Saqlain Ahmad <123837216+SaqlainAhmad786@users.noreply.github.com>
2024-12-14 13:32:25 +0530

    the

--- src/components/AddDefaulter.jsx
+++ src/components/AddDefaulter.jsx
@@ -4,9 +4,6 @@ function AddDefaulter() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         const formData = new FormData(e.target);
-        // const data = Object.fromEntries(formData);
-        // const postData = { ...data, user_id: localStorage.getItem('userId') };
-        // console.log(postData)
         formData.append('user_id', localStorage.getItem('userId'));
 
         try {
