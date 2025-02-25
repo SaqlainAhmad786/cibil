1091adcf5fb9c552dce380d712026103d388c7c9
Aman Verma <amanverma0428@gmail.com>
2025-02-25 23:13:22 +0530

    bug/fixed

--- src/contexts/authContext.jsx
+++ src/contexts/authContext.jsx
@@ -1,83 +1,109 @@
-import { createContext, useContext, useEffect, useState } from 'react';
-import axios from 'axios';
+import { createContext, useContext, useEffect, useState } from "react";
+import axios from "axios";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-    const [userData, setUserData] = useState({});
-    const [defaultersList, setDefaultersList] = useState([]);
-    const [userDefaultersList, setUserDefaultersList] = useState([]);
-    const [userLoading, setUserLoading] = useState(false);
-    const [defaultersLoading, setDefaultersLoading] = useState(false);
-    const token = localStorage.getItem('token');
+	const [userData, setUserData] = useState({});
+	const [defaultersList, setDefaultersList] = useState([]);
+	const [userDefaultersList, setUserDefaultersList] = useState([]);
+	const [userLoading, setUserLoading] = useState(false);
+	const [defaultersLoading, setDefaultersLoading] = useState(false);
+	const token = localStorage.getItem("token");
 
-    useEffect(() => {
-        if (token) {
-            refreshUserData();
-            getDefaultersList();
-            getUserDefaultersList();
-        }
-    }, [token]);
+	useEffect(() => {
+		if (token) {
+			refreshUserData();
+			getDefaultersList();
+			getUserDefaultersList();
+		}
+	}, [token]);
 
-    async function getDefaultersList() {
-        setDefaultersLoading(true);
-        try {
-            await axios.get(`${import.meta.env.VITE_BASE_URL}/defaulter`, { headers: { Authorization: `Bearer ${token}` } }).then(res => {
-                setDefaultersList(res.data.allDefaulter);
-                setDefaultersLoading(false);
-            })
-        } catch (error) {
-            console.error('Error fetching defaulter list:', error);
-            setDefaultersLoading(false);
-        }
-    }
+	async function getDefaultersList() {
+		setDefaultersLoading(true);
+		try {
+			await axios
+				.get(`${import.meta.env.VITE_BASE_URL}/defaulter`, {
+					headers: { Authorization: `Bearer ${token}` },
+				})
+				.then((res) => {
+					setDefaultersList(res.data.allDefaulter);
+					setDefaultersLoading(false);
+				});
+		} catch (error) {
+			console.error("Error fetching defaulter list:", error);
+			setDefaultersLoading(false);
+		}
+	}
 
-    async function getUserData(token) {
-        setUserLoading(true);
-        try {
-            await axios.get(`${import.meta.env.VITE_BASE_URL}/user/current-user`, { headers: { Authorization: `Bearer ${token}` } }).then(res => {
-                setUserData(res.data.user);
-                setUserLoading(false);
-            })
-        } catch (error) {
-            console.error('Error fetching user data:', error);
-            setUserLoading(false);
-        }
-    }
+	async function getUserData(token) {
+		setUserLoading(true);
+		try {
+			await axios
+				.get(`${import.meta.env.VITE_BASE_URL}/user/current-user`, {
+					headers: { Authorization: `Bearer ${token}` },
+				})
+				.then((res) => {
+					setUserData(res.data.user);
+					setUserLoading(false);
+				});
+		} catch (error) {
+			console.error("Error fetching user data:", error);
+			setUserLoading(false);
+		}
+	}
 
-    async function getUserDefaultersList() {
-        try {
-            await axios.get(`${import.meta.env.VITE_BASE_URL}/defaulter/defaulter-by-current-user`, { headers: { Authorization: `Bearer ${token}` } }).then(res => {
-                setUserDefaultersList(res.data);
-            })
-        } catch (error) {
-            console.error('Error fetching user data:', error);
-        }
-    }
+	async function getUserDefaultersList() {
+		try {
+			await axios
+				.get(
+					`${
+						import.meta.env.VITE_BASE_URL
+					}/defaulter/defaulter-by-current-user`,
+					{ headers: { Authorization: `Bearer ${token}` } }
+				)
+				.then((res) => {
+					setUserDefaultersList(res.data.defaulters);
+				});
+		} catch (error) {
+			console.error("Error fetching user data:", error);
+		}
+	}
 
-    const refreshUserData = () => {
-        if (!token) return;
-        getUserData(token);
-    };
+	const refreshUserData = () => {
+		if (!token) return;
+		getUserData(token);
+	};
 
-    const refreshDefaultersList = () => {
-        getDefaultersList();
-        // getUserDefaultersList();
-    };
+	const refreshDefaultersList = () => {
+		getDefaultersList();
+		// getUserDefaultersList();
+	};
 
-    const logout = () => {
-        localStorage.removeItem('token');
-        localStorage.removeItem('userId');
-        setUserData({});
-        setDefaultersList([]);
-        setUserDefaultersList([]);
-    };
+	const logout = () => {
+		localStorage.removeItem("token");
+		localStorage.removeItem("userId");
+		setUserData({});
+		setDefaultersList([]);
+		setUserDefaultersList([]);
+	};
 
-    return (
-        <AuthContext.Provider value={{ refreshUserData, userDefaultersList, refreshDefaultersList, logout, userData, defaultersList, userLoading, defaultersLoading }}>
-            {children}
-        </AuthContext.Provider>
-    );
+	return (
+		<AuthContext.Provider
+			value={{
+				refreshUserData,
+				userDefaultersList,
+				refreshDefaultersList,
+				logout,
+				userData,
+				defaultersList,
+				userLoading,
+				defaultersLoading,
+			}}
+		>
+			{children}
+		</AuthContext.Provider>
+	);
 };
 
 export const useAuth = () => useContext(AuthContext);
